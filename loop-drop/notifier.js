fcb4935d1c5468d64357c1060770b39c875a3321
Matt McKegg <matt@wetsand.co.nz>
2015-06-28 20:27:33 +1200

    drop the price, tweak the open source propaganda, add note about slack chat

--- loop-drop/notifier.js
+++ loop-drop/notifier.js
@@ -27,13 +27,17 @@ module.exports = function(detail) {
 
           'Loop Drop is still experimental and very much in active development. New versions are being released all the time, and you\'ll likely see some major changes!\n\n' +
 
-          'If you encounter bugs, please let let me know by email. I\'ll try to address them as soon as possible.\n\n' +
+          'If you encounter bugs, please let let me know. I\'ll try to address them as soon as possible.\n\n' +
           
+          'Also keep an eye out for an email invite to join the Loop Drop chatroom on Slack!\n\n' +
+
           'Over the next little while, I\'ll be uploading a bunch of tutorial videos to the YouTube channel. Hopefully these will be helpful.\n' +
           'https://www.youtube.com/channel/UC2wAgvZBPlRoqHRQ7vS0RZg\n\n' +
 
           'Download Link: ' + root + '/download/' + detail.TRANSACTIONID + '\n' +
           'You can use this link to redownload the app again in the future.\n\n' +
+
+          'Feel free to share this download link with your friends if they want to try Loop Drop! Just don\'t post it publicly.' +
 
           '- Matt' + '\n\n' +
 
